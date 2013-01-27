e73584203807b084f4085fc40ef831bae62573e9
Jordan Santell <jsantell@gmail.com>
2013-01-26 21:33:35 -0500

    Styling progress

--- template.js
+++ template.js
@@ -1,17 +1,18 @@
 module.exports =
 '<div class="rack-container rack-container-{{name}}">' +
   '<div class="rack-logo"></div>' +
+  '<div class="rack-logo-text">Web Audio Component</div>' +
   '<div class="rack-name">{{name}}</div>' +
-  '<div class="rack-params">' +
+  '<ul class="rack-params">' +
     '{{#params}}' +
-      '<div class="rack-param">' +
+      '<li class="rack-param">' +
         '<div class="rack-param-name">{{name}}</div>' +
         '<div class="rack-param-control">' +
           '<input type="number" class="rack-param-input-{{name}}" ' +
             'data-type="{{type}}" data-name="{{name}}" />' +
         '</div>' +
         '<div class="rack-param-value rack-param-value-{{name}}">{{value}}</div>' +
-      '</div>' +
+      '</li>' +
     '{{/params}}' +
-  '</div>' +
+  '</ul>' +
 '</div>';
