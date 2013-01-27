ff77b2cc40d4264cfd5bcb48292a0d57e5a6734c
Jordan Santell <jsantell@gmail.com>
2013-01-26 22:09:50 -0500

    More styling love

--- template.js
+++ template.js
@@ -1,8 +1,10 @@
 module.exports =
 '<div class="rack-container rack-container-{{name}}">' +
   '<div class="rack-logo"></div>' +
-  '<div class="rack-logo-text">Web Audio Component</div>' +
-  '<div class="rack-name">{{name}}</div>' +
+  '<div class="rack-text">' +
+    '<div class="rack-name">{{name}}</div>' +
+    '<div class="rack-logo-text">Web Audio Component</div>' +
+  '</div>' +
   '<ul class="rack-params">' +
     '{{#params}}' +
       '<li class="rack-param">' +
